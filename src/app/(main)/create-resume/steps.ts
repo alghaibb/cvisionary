b19c73052b689063a00ee322f191243765610c00
Moudi <mjaderi97@gmail.com>
2024-12-22 19:31:50 +1100

    Remove SummaryForm from resume creation steps

--- src/app/(main)/create-resume/steps.ts
+++ src/app/(main)/create-resume/steps.ts
@@ -4,7 +4,7 @@ import WorkExperienceForm from "./forms/WorkExperienceForm";
 import EducationForm from "./forms/EducationForm";
 import { CreateResumeProps } from "@/types/create-resume";
 import SkillsForm from "./forms/SkillsForm";
-import SummaryForm from "./forms/SummaryForm";
+// import SummaryForm from "./forms/SummaryForm";
 
 export const steps: {
   title: string;
@@ -16,5 +16,5 @@ export const steps: {
     { title: "Work Experience", component: WorkExperienceForm, key: "work-experience" },
     { title: "Education", component: EducationForm, key: "education" },
     { title: "Skills", component: SkillsForm, key: "skills" },
-    { title: "Summary", component: SummaryForm, key: "summary" },
+    // { title: "Summary", component: SummaryForm, key: "summary" },
   ];
