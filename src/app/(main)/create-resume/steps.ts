8e13d616f667aff3e18a08355e688301eb915f87
Moudi <mjaderi97@gmail.com>
2024-12-22 19:44:49 +1100

    Comment out EducationForm in resume creation steps

--- src/app/(main)/create-resume/steps.ts
+++ src/app/(main)/create-resume/steps.ts
@@ -1,8 +1,8 @@
+import { CreateResumeProps } from "@/types/create-resume";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
-import EducationForm from "./forms/EducationForm";
-import { CreateResumeProps } from "@/types/create-resume";
+// import EducationForm from "./forms/EducationForm";
 // import SkillsForm from "./forms/SkillsForm";
 // import SummaryForm from "./forms/SummaryForm";
 
@@ -14,7 +14,7 @@ export const steps: {
     { title: "General Information", component: GeneralInfoForm, key: "general-info" },
     { title: "Personal Information", component: PersonalInfoForm, key: "personal-info" },
     { title: "Work Experience", component: WorkExperienceForm, key: "work-experience" },
-    { title: "Education", component: EducationForm, key: "education" },
+    // { title: "Education", component: EducationForm, key: "education" },
     // { title: "Skills", component: SkillsForm, key: "skills" },
     // { title: "Summary", component: SummaryForm, key: "summary" },
   ];
