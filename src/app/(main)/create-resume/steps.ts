39bc13e5980a7523c67d8674e814dbc503953414
Moudi <mjaderi97@gmail.com>
2024-12-22 19:42:11 +1100

    Comment out SkillsForm and SummaryForm in resume creation steps

--- src/app/(main)/create-resume/steps.ts
+++ src/app/(main)/create-resume/steps.ts
@@ -3,7 +3,7 @@ import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
 import EducationForm from "./forms/EducationForm";
 import { CreateResumeProps } from "@/types/create-resume";
-import SkillsForm from "./forms/SkillsForm";
+// import SkillsForm from "./forms/SkillsForm";
 // import SummaryForm from "./forms/SummaryForm";
 
 export const steps: {
@@ -15,6 +15,6 @@ export const steps: {
     { title: "Personal Information", component: PersonalInfoForm, key: "personal-info" },
     { title: "Work Experience", component: WorkExperienceForm, key: "work-experience" },
     { title: "Education", component: EducationForm, key: "education" },
-    { title: "Skills", component: SkillsForm, key: "skills" },
+    // { title: "Skills", component: SkillsForm, key: "skills" },
     // { title: "Summary", component: SummaryForm, key: "summary" },
   ];
