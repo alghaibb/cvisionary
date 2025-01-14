30844336fff552c908957a31b61a46f98f8d99e9
Moudi <mjaderi97@gmail.com>
2025-01-14 19:49:34 +1100

    Add pricing section to homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import Features from "./_components/Features";
 import Hero from "./_components/Hero";
+import Pricing from "./_components/Pricing";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col">
       <Hero />
       <Features />
+      <Pricing />
     </main>
   );
 }
